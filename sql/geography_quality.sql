d3a4dbd100a5f89a224a07552f080495e5abea66
Swikritip <149959593+Swikritip@users.noreply.github.com>
2026-09-22 20:18:04 -0400

    Update geography_quality.sql

--- sql/geography_quality.sql
+++ sql/geography_quality.sql
@@ -1,14 +1,4 @@
 SELECT
-    COUNT(*) AS records_with_missing_data
-FROM samples.bakehouse.sales_customers
-WHERE customerID IS NULL
-   OR city IS NULL
-   OR state IS NULL
-   OR country IS NULL
-   OR continent IS NULL
-   OR postal_zip_code IS NULL
-   OR gender IS NULL;
-SELECT
     country,
     COUNT(*) AS geographic_inconsistencies
 FROM samples.bakehouse.sales_customers
