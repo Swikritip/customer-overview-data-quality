396f525828d57e4c9a30e498f26d17d2c1075d00
Swikritip <149959593+Swikritip@users.noreply.github.com>
2026-09-22 20:17:47 -0400

    Update geography_quality.sql

--- sql/geography_quality.sql
+++ sql/geography_quality.sql
@@ -8,3 +8,10 @@ WHERE customerID IS NULL
    OR continent IS NULL
    OR postal_zip_code IS NULL
    OR gender IS NULL;
+SELECT
+    country,
+    COUNT(*) AS geographic_inconsistencies
+FROM samples.bakehouse.sales_customers
+WHERE country <> 'USA'
+GROUP BY country
+ORDER BY geographic_inconsistencies DESC;
