27274f1ea52042351991e4a4fb5f4e9dee594444
Swikritip <149959593+Swikritip@users.noreply.github.com>
2026-09-22 20:18:16 -0400

    Update missing_data_check.sql

--- sql/missing_data_check.sql
+++ sql/missing_data_check.sql
@@ -1,7 +1,10 @@
 SELECT
-    country,
-    COUNT(*) AS geographic_inconsistencies
+    COUNT(*) AS records_with_missing_data
 FROM samples.bakehouse.sales_customers
-WHERE country <> 'USA'
-GROUP BY country
-ORDER BY geographic_inconsistencies DESC;
+WHERE customerID IS NULL
+   OR city IS NULL
+   OR state IS NULL
+   OR country IS NULL
+   OR continent IS NULL
+   OR postal_zip_code IS NULL
+   OR gender IS NULL;
